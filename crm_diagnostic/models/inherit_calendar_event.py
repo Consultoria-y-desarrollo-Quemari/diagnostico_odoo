33b355d246097d6351f49c13f73a62b901842693
Ernesto García Medina <egarcia@quemari.com>
2021-05-04 11:27:25 -0500

    Se remueve write en el create para evitar problemas con usuarios

--- crm_diagnostic/models/inherit_calendar_event.py
+++ crm_diagnostic/models/inherit_calendar_event.py
@@ -1,15 +1,8 @@
 # -*- encoding: utf-8 -*-
 from odoo import models, fields, api
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
-
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        results = super(CalendarEvent, self).create(vals_list)
-        for res in results:
-            res.write({'allday': False})
-            res.write({'duration': 2})
-        return results

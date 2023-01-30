9ad27e982fd3d9e662ace56da666379826dc317f
Gavii <agavariat@gmail.com>
2023-01-30 11:29:55 -0500

    binary attached false

--- crm_diagnostic/models/crm_attention_plan.py
+++ crm_diagnostic/models/crm_attention_plan.py
@@ -370,5 +370,5 @@ class CrmAttentionPlanLinesBitacora(models.Model):
     tipo_actividad1 = fields.Char(string="Tipo de Actividad")
     registro_avance = fields.Char()
     observaciones = fields.Char()
-    adjunto = fields.Binary()
+    adjunto = fields.Binary(attachment=False)
     tipo_actividad_ids = fields.Many2one('crm.lead.type_activity', string="Tipo de Actividad") 

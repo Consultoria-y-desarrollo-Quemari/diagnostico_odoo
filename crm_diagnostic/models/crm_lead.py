02854931ad6fe14e3dcbea0157bdd2d1476fccc1
Gavii <agavariat@gmail.com>
2023-02-23 13:44:15 -0500

    remove super user

--- crm_diagnostic/models/crm_lead.py
+++ crm_diagnostic/models/crm_lead.py
@@ -479,7 +479,7 @@ class CrmLead(models.Model):
     def finalizar_caso(self):
         _logger.info("este es el bon de finalizacion")
         five_stage = self.get_stage('quinto_encuentro')
-        self.with_user(SUPERUSER_ID).stage_id = five_stage
+        self.stage_id = five_stage
 
 
     @api.depends('child_ids.planned_hours')
